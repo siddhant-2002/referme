ec0ea0be3f79d12144a49e9e82e04edebc84d3a7
siddhant-2002 <siddeshmukh711@gmail.com>
2024-04-05 19:44:05 +0530

    new commits

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,7 +3,7 @@ import Contactus from "./Contactus";
 import Aboutus from "./Aboutus";
 import Home from "./Home";
 import Features from "./Features";
-
+import Mission from "./Mission";
 
 const Body = () => {
 	return (
@@ -11,22 +11,11 @@ const Body = () => {
 
 			<Home/>
 			
-			{/* <div className="flex justify-between p-4 bg-teal">
-				<p className="text-lg font-semibold text-off-white">
-					This is some text on the left.
-				</p>
-				<p className="text-lg font-semibold text-off-white">
-					An online platform designed specifically for engineering
-					students seeking comprehensive study materials, handwritten
-					notes, and practical codes. We understand the challenges of
-					engineering education and are committed to providing a
-					platform that caters to your academic needs with precision
-					and excellence.
-				</p>
-			</div> */}
+		
 
 			<Aboutus/>
 
+			<Mission/>
 
 			<Contactus/>
 
