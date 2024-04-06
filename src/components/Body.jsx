a2f93b73b07a1adafbb5c17f8006a1708ed66968
siddhant-2002 <siddeshmukh711@gmail.com>
2024-04-06 20:48:28 +0530

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -10,33 +10,14 @@ const Body = () => {
 		<div className="min-h-screen bg-off-white text-teal">
 
 			<Home/>
-			{/* <div className="flex justify-between p-4 bg-teal">
-				<p className="text-lg font-semibold text-off-white">
-					This is some text on the left.
-				</p>
-				<p className="text-lg font-semibold text-off-white">
-					An online platform designed specifically for engineering
-					students seeking comprehensive study materials, handwritten
-					notes, and practical codes. We understand the challenges of
-					engineering education and are committed to providing a
-					platform that caters to your academic needs with precision
-					and excellence.
-				</p>
-			</div> */}
-			{/* features */}
-			<Features />
+			
+			<Aboutus/>
 		
 			<Mission/>
 
-			<Aboutus/>
-
-			
+			<Features/>
 
 			<Contactus/>
-
-			
-
-			
 
 		</div>
 
