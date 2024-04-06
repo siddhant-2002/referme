d3410639ed58af06230ebfbf7b2e9cd3590796ef
Yash Chitale <114438423+yashchitale96@users.noreply.github.com>
2024-04-06 23:56:32 +0530

    change

--- src/components/Year.jsx
+++ src/components/Year.jsx
@@ -26,7 +26,7 @@ const Year = () => {
            </button>
            {expanded[index] && (
                <div className="mt-2 space-y-2 rounded-md transition-all duration-200 ease-in-out transform ">
-               <button className="w-full text-left bg-dark-blue hover:bg-teal text-off-white py-2 px-4 rounded-md font-semibold text-lg transition-colors duration-200 hover:scale-105">Semistar 1</button>
+               <button className="w-full text-left bg-dark-blue hover:bg-teal text-off-white py-2 px-4 rounded-md font-semibold text-lg transition-colors duration-200 hover:scale-105" onClick={() => navigate('/Subject')} >Semistar 1</button>
                <button className="w-full text-left bg-dark-blue hover:bg-teal text-off-white py-2 px-4 rounded-md font-semibold text-lg transition-colors duration-200 hover:scale-105">Semister 2</button>
            </div>
            )}
