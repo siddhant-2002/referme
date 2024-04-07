b1e58de490e04e5e83426d76609fbf41a210cde7
Yash Chitale <114438423+yashchitale96@users.noreply.github.com>
2024-04-07 20:04:05 +0530

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
-  const handleScroll = (event, id) => {
-    event.preventDefault();
-    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
-  };
+  const location = useLocation();
+
+  React.useEffect(() => {
+    if (location.hash) {
+      let elem = document.getElementById(location.hash.slice(1))
+      if (elem) {
+        elem.scrollIntoView({behavior: "smooth"})
+      }
+    } else {
+      window.scrollTo({top: 0, left: 0, behavior: "smooth"})
+    }
+  }, [location,]);
 
   return (
     <header className="w-full z-10 flex items-center justify-between px-8 py-4 shadow-md bg-dark-blue">
@@ -13,10 +22,10 @@ const Header = () => {
         <nav className="flex flex-col sm:flex-row sm:justify-between">
           <ul className="flex flex-col sm:flex-row gap-8 justify-between">
             <li><a href="/" className="text-lg hover:text-light-teal text-off-white">Home</a></li>
-            <li><a href="#about-us" onClick={(event) => handleScroll(event, 'about-us')} className="text-lg hover:text-light-teal text-off-white">About Us</a></li>
-            <li><a href="#mission" onClick={(event) => handleScroll(event, 'mission')} className="text-lg hover:text-light-teal text-off-white">Mission</a></li>
-            <li><a href="#features" onClick={(event) => handleScroll(event, 'features')} className="text-lg hover:text-light-teal text-off-white">Features</a></li>
-            <li><a href="#contactus" onClick={(event) => handleScroll(event, 'contactus')} className="text-lg hover:text-light-teal text-off-white">Contact Us</a></li>
+            <li><Link to="/#about-us" className="text-lg hover:text-light-teal text-off-white">About Us</Link></li>
+            <li><Link to="/#mission" className="text-lg hover:text-light-teal text-off-white">Mission</Link></li>
+            <li><Link to="/#features" className="text-lg hover:text-light-teal text-off-white">Features</Link></li>
+            <li><Link to="/#contactus" className="text-lg hover:text-light-teal text-off-white">Contact Us</Link></li>
           </ul>
         </nav>
       </div>
