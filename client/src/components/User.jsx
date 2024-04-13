2279051f13419ccd140f4c1a7ad56bd4e63fdb86
siddhant-2002 <siddeshmukh711@gmail.com>
2024-04-13 23:08:33 +0530

    new commit

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,29 +1,98 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import app from '../firebase';
 
-const User = () => {
-    const [selectedFile, setSelectedFile] = useState(null);
+const Upload = () => {
 
-    const handleFileChange = (event) => {
-        setSelectedFile(event.target.files[0]);
-    };
+  
+    const [inputs, setInputs] = useState({});
 
-    const handleUpload = () => {
-        // Implement your file upload logic here
-        if (selectedFile) {
-            // Upload the selected file to the server
-            console.log('Uploading file:', selectedFile);
-        } else {
-            console.log('No file selected');
+    const [pdf, setPdf] = useState(undefined);
+    const [pdfprec, setPdfPerc] = useState(0);
+
+    useEffect(() => {
+        pdf && uploadFile(pdf, "pdfurl");
+    }, [pdf]);
+
+
+    const uploadFile = (file, fileType) => {
+        const storage = getStorage(app);
+        const folder = fileType === "pdfurl" ? "pdfs/" : "";
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, folder + fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on("state_changed", (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            if (fileType === "pdfurl") {
+                setPdfPerc(Math.round(progress))
+            }
+
+        },
+            (error) => {
+                console.log(error);
+                switch (error.code) {
+                    case "storage/unauthorized":
+                        // User doesn't have permission to access the object
+                        console.log(error);
+                        break;
+                    case "storage/canceled":
+                        // User canceled the upload
+                        break;
+                    case "storage/unknown":
+                        // Unknown error occurred, inspect error.serverResponse
+                        break;
+                    default:
+                        break;
+                }
+            },
+            () => {
+                // Upload completed successfully, now we can get the download URL
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    // console.log('DownloadURL - ', downloadURL); -------------herre is the download url
+                    setInputs((prev) => {
+                        return {
+                            ...prev,
+                            [fileType]: downloadURL,
+                        };
+                    });
+                });
+            }
+        );
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post(`http://localhost:5000/api/videos`, { ...inputs });
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
         }
     };
 
     return (
-        <div>
-            <h1>Admin Page</h1>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleUpload}>Upload</button>
+        <div className="upload">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="pdf">Upload File:</label> {pdfprec > 0 && "Uploading: " + pdfprec + "%"}
+                    <br />
+                    <input
+                        type="file"
+                        accept="pdf/*"
+                        id="pdf"
+                        onChange={(e) => setPdf((prev) => e.target.files[0])}
+                    />
+                </div>
+                <br />
+              
+                <br />
+                {/* <button type="submit">Upload</button> */}
+            </form>
         </div>
-    );
-};
+    )
+}
 
-export default User;
+export default Upload
