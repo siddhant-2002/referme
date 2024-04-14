5f7cf0a72c29baff8b92dd3a724fd46af4367155
Yash Chitale <114438423+yashchitale96@users.noreply.github.com>
2024-04-14 09:40:49 +0530

    imp

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -76,23 +76,25 @@ const Upload = () => {
   };
 
   return (
-    <div className="upload">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="pdf">Pdf:</label> {pdfPerc > 0 && "Uploading: " + pdfPerc + "%"}
-          <br />
+    <div className="flex items-center justify-center h-screen bg-off-white">
+      <form onSubmit={handleSubmit} className="w-1/2 bg-white rounded-lg shadow-2xl p-6 m-4">
+        <div className="mb-4">
+          <label htmlFor="pdf" className="block text-gray-700 text-2xl font-bold mb-2 ">Pdf:</label>
+          {pdfPerc > 0 && <p className="text-gray-700 text-sm mb-2">Uploading: {pdfPerc}%</p>}
           <input
             type="file"
             accept="Pdf/*"
             id="pdf"
             onChange={(e) => setPdf((prev) => e.target.files[0])}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
           />
         </div>
-        <br />
-        <button type="submit">Upload</button>
+        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+          Upload
+        </button>
       </form>
     </div>
-  )
+  );
 }
 
-export default Upload
+export default Upload;
